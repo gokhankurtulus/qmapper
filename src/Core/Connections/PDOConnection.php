9b00715d9c78fde3ec8501e6bcae18a3f5f56668
gokhankurtulus <gokhankurtulus@icloud.com>
2023-09-28 05:56:00 +0300

    update Connections

--- src/Core/Connections/PDOConnection.php
+++ src/Core/Connections/PDOConnection.php
@@ -8,100 +8,63 @@
 namespace QMapper\Core\Connections;
 
 use PDO;
-use PDOException;
+use QMapper\Enums\MapperStringTemplate;
 use QMapper\Exceptions\DatabaseException;
-use QMapper\Interfaces\DBMSInterface;
+use QMapper\Interfaces\IConnection;
 
-class PDOConnection implements DBMSInterface
+abstract class PDOConnection implements IConnection
 {
-    protected PDO $pdo;
-    protected string $engine = "";
-    protected string $charset = "";
-    protected string $collate = "";
-    protected string $query = '';
-    protected array $bindings = [];
+    protected ?PDO $pdo = null;
+
+
+    abstract public function initialize(): void;
+
+    public function terminate(): void
+    {
+        $this->setPDO(null);
+    }
 
     /**
      * @throws DatabaseException
      */
     public function __construct()
     {
-        $this->setEngine($_ENV['DB_ENGINE']);
-        $this->setCharset($_ENV['DB_CHARSET']);
-        $this->setCollate($_ENV['DB_COLLATE']);
         try {
-            $this->setPDO();
-        } catch (PDOException $exception) {
+            if (!extension_loaded('pdo')) throw new DatabaseException(MapperStringTemplate::EXTENSION_REQUIRED->get('pdo'));
+            $this->initialize();
+        } catch (\Exception|\Throwable $exception) {
             throw new DatabaseException($exception->getMessage());
         }
     }
 
+    public function __destruct()
+    {
+        $this->terminate();
+    }
+
     /**
-     * @return PDO
+     * @return PDO|null
      */
-    public function getPDO(): PDO
+    protected function getPDO(): ?PDO
     {
         return $this->pdo;
     }
 
     /**
-     * @return bool
+     * @param PDO|null $pdo
+     * @return void
      */
-    public function setPDO(): bool
+    protected function setPDO(?PDO $pdo): void
     {
-        $this->pdo = new PDO($_ENV['DB_DSN'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], [
+        $this->pdo = $pdo;
+    }
+
+    protected function createPDO(string $dsn, string $user = "", string $password = ""): PDO
+    {
+        return new PDO($dsn, $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES => false
         ]);
-        return true;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEngine(): string
-    {
-        return $this->engine;
-    }
-
-    /**
-     * @param string $engine
-     */
-    public function setEngine(string $engine): void
-    {
-        $this->engine = $engine;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCharset(): string
-    {
-        return $this->charset;
-    }
-
-    /**
-     * @param string $charset
-     */
-    public function setCharset(string $charset): void
-    {
-        $this->charset = $charset;
-    }
-
-    /**
-     * @return string
-     */
-    public function getCollate(): string
-    {
-        return $this->collate;
-    }
-
-    /**
-     * @param string $collate
-     */
-    public function setCollate(string $collate): void
-    {
-        $this->collate = $collate;
     }
 }
